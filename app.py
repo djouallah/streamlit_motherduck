9ce2fa140188491bd43bef3774c09764547d5ca7
Mimoune <djouallah@users.noreply.github.com>
2023-08-09 09:41:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ st.set_page_config(
                   )
 col1, col2 = st.columns([3, 1])
 ################################
-con = duckdb.connect(f'''md:?token={st.secrets["md_token"]}''',read_only=True)
+@st.cache_resource(ttl=5*60)
+def define_connection():
+    con = duckdb.connect(f'''md:?token={st.secrets["md_token"]}''',read_only=True)
+    return con
+con=define_connection()
 ###############################
 SQL = st.text_input('Write a SQL Query', 'SHOW DATABASES')
 try :
@@ -21,4 +25,3 @@ try :
   st.write(df)
 except Exception as er:
  st.write(df = pd.DataFrame([{'error':er}]))
-con.close()
